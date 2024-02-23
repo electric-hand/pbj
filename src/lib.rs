48761703e7a758b1b0a97b05c372705096d37f55
Joshua Reynolds <jreynolds@electrichand.com>
2024-02-23 21:35:23 +0100

    add initial code for deserializing project initializer toml

---
+++ src/lib.rs
@@ -0,0 +1,59 @@
+use serde::Deserialize;
+
+type Path = String;
+
+#[derive(Debug, Deserialize)]
+pub struct TestDrivenConfig {
+    language: Language,
+    project: Project,
+    code: Code,
+    config: Vec<FileSpec>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Language {
+    binary: String,
+    version: String,
+    name: String,
+    file_extension: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Project {
+    dependencies: Vec<String>,
+    dev_dependencies: Vec<String>,
+    tool: ProjectTool,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ProjectTool {
+    binary: String,
+    makes_base_directory: bool,
+    commands: ProjectToolCommands,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ProjectToolCommands {
+    initialize: Vec<String>,
+    add_development_dependency: Vec<String>,
+    add_dependency: Vec<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Code {
+    directories: CodeDirectories,
+    source: Vec<FileSpec>,
+    test: Vec<FileSpec>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CodeDirectories {
+    source: Path,
+    test: Path,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct FileSpec {
+    contents: String,
+    file: Path,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use tddme::TestDrivenConfig;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let toml_file = fs::read_to_string("./explore/python.toml").expect("this to work");
+    let decoded: TestDrivenConfig = toml::from_str(&toml_file).expect("bad toml");
+    println!("{:#?}", decoded);
 }
