56c22cf0f800e410d8dc984718edd53fc5ca62df
Joshua Reynolds <jreynolds@electrichand.com>
2024-05-10 20:57:10 +0200

    fix: fixed name for default config contents lookup

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,6 @@
 pub const CONFIG_FILE_NAME: &str = "config.toml";
+pub const PYTHON_TEMPLATE_KEY: &str = "python";
+pub const TYPESCRIPT_TEMPLATE_KEY: &str = "typescript";
 pub const DEFAULT_TEMPLATE: &str = "python";
 pub const DEFAULT_VARIANT_VALUE: &str = "default";
 pub const DEFAULT_PREFIX_SEPARATOR: &str = "_";
--- src/files.rs
+++ src/files.rs
@@ -2,9 +2,11 @@ use std::{fs::read_to_string, path::PathBuf};
 
 use log::{error, info, trace};
 
-const PYTHON_TOML: &str = include_str!("../templates/python.toml");
-const TYPESCRIPT: &str = include_str!("../templates/typescript.toml");
-const CONFIG: &str = include_str!("../default_config.toml");
+use crate::constants::{CONFIG_FILE_NAME, PYTHON_TEMPLATE_KEY, TYPESCRIPT_TEMPLATE_KEY};
+
+const PYTHON_TEMPLATE_CONTENTS: &str = include_str!("../templates/python.toml");
+const TYPESCRIPT_TEMPLATE_CONTENTS: &str = include_str!("../templates/typescript.toml");
+const DEFAULT_CONFIG_CONTENTS: &str = include_str!("../default_config.toml");
 const CONFIG_DIR: &str = ".config";
 
 const APP_DIR: &str = "pbj";
@@ -19,9 +21,9 @@ fn get_template_path(template: &str) -> PathBuf {
 pub fn get_default_file_contents(key: &str) -> Option<&str> {
 
     let result = match key {
-        "python" => Some(PYTHON_TOML),
-        "typescript" => Some(TYPESCRIPT),
-        "config" => Some(CONFIG),
+        PYTHON_TEMPLATE_KEY => Some(PYTHON_TEMPLATE_CONTENTS),
+        TYPESCRIPT_TEMPLATE_KEY => Some(TYPESCRIPT_TEMPLATE_CONTENTS),
+        CONFIG_FILE_NAME => Some(DEFAULT_CONFIG_CONTENTS),
         _ => None,
     };
 
